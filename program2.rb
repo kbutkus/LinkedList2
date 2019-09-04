019b9c120c50978bed3077d8b0edaaa62ca4b3f4
kbutkus@gmail.com <kbutkus@gmail.com>
2019-09-04 23:13:32 +0000

    updates

--- program2.rb
+++ program2.rb
@@ -1,7 +1,7 @@
 load "LinkedList.rb"
 load "stack.rb"
 
-def reverse_list(list)
+def reverse_list(list, previous=nil)
   stack = Stack.new
   while list
     stack.push(list.value)
@@ -34,22 +34,3 @@ puts "-------"
 revlist = reverse_list(node3)
 
 print_values(revlist)
-# Creates a new Stack object
-
-stack = Stack.new
-
-# Pushes (adds) the number 1 to the empty stack
-stack.push(1)
-
-# Pushes the number 2 to the TOP of the stack
-stack.push(2)
-
-# Pops (removes) the TOP number from the stack (2)
-puts stack.pop
-
-# Pops the remaining number from the stack (1)
-puts stack.pop
-
-# Tries to pop the TOP number from the stack,
-# but it is empty so it returns nil
-puts stack.pop
